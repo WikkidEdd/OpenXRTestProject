953e4b33b90b6bca3497e296d2120a0e6c09b94c
Edd Smith <edd@fracturereality.io>
2022-03-14 22:29:30 +0100

    Added ARR and MRC PV Camera script

---
+++ Assets/MRCPVCameraSetting.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class MRCPVCameraSetting : MonoBehaviour
+{
+    public bool _Enable;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+#if WINDOWS_UWP
+
+        Debug.Log("Attempting to Apply PV Camera setting for MRC");
+        // If the default display has configuration for a PhotoVideoCamera, we want to enable it
+        global::Windows.Graphics.Holographic.HolographicViewConfiguration viewConfiguration = global::Windows.Graphics.Holographic.HolographicDisplay.GetDefault()?.TryGetViewConfiguration(global::Windows.Graphics.Holographic.HolographicViewConfigurationKind.PhotoVideoCamera);
+                
+        if (viewConfiguration != null)
+        {
+            Debug.Log("Setting viewConfiguration.IsEnabled to " + _Enable);
+            viewConfiguration.IsEnabled = _Enable;
+        }
+
+#endif // WINDOWS_UWP
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
